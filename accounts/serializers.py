ec34c266ff876577ab41f5512183f00b8c064add
Benjamin Donnaloia <OwlFace@Benjamins-MacBook.local>
2015-10-14 15:52:51 -0700

    okay

--- accounts/serializers.py
+++
@@ -1,7 +0,0 @@
-from accounts.models import Entry
-from rest_framework import serializers
-
-class EntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Entry
-        fields = ('headline', 'body_text', 'author', 'pub_date', 'zipcode', 'price1', 'price2', 'price3', 'price4', 'price5', 'entrytype', 'item_picture')
